0e4e2e9fa48fc28ac22d278ff907bcf85a7e4509
Eduardo Alfonso Sanchez <eddie.alfonso@gmail.com>
2017-12-27 12:46:20 -0500

    use fieldset ui from primeNG

--- angular-caos/src/app/admin/admin.module.ts
+++ angular-caos/src/app/admin/admin.module.ts
@@ -3,9 +3,12 @@ import {CommonModule} from '@angular/common';
 import {InsertUserComponent} from './insert-user/insert-user.component';
 import {ListUserComponent} from './list-user/list-user.component';
 
+import {FieldsetModule} from 'primeng/primeng';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FieldsetModule
   ],
   declarations: [InsertUserComponent, ListUserComponent],
   exports: [InsertUserComponent, ListUserComponent],
