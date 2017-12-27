ffa0d2d0c8f9516386cebd73f0c3d33fc6f6b97b
Eduardo Alfonso Sanchez <eddie.alfonso@gmail.com>
2017-12-27 12:45:01 -0500

    add BrowserAnimationsModule in root module because is common for the mayority of the component

--- angular-caos/src/app/app.module.ts
+++ angular-caos/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
 
 
 import { AppComponent } from './app.component';
@@ -14,7 +16,8 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
