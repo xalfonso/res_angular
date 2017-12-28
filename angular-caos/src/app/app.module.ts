ad6a8a7b98b473573744967df0e1420231b9c37a
Eduardo Alfonso Sanchez <eddie.alfonso@gmail.com>
2017-12-28 17:27:37 -0500

    working...

--- angular-caos/src/app/app.module.ts
+++ angular-caos/src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
 import {GrowlModule} from 'primeng/primeng';
 import {ButtonModule} from 'primeng/primeng';
 import {MessageService} from 'primeng/components/common/messageservice';
@@ -25,4 +25,5 @@ import {MessageService} from 'primeng/components/common/messageservice';
   providers: [MessageService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
